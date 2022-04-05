606b42039df4ae9679dd1de0138ce2e6af9323f1
Mono <MonoCode22@gmail.com>
2022-04-05 16:33:25 +0300

    Added serializer and deserializer

---
+++ Frontend/Assets/Scripts/General/Deserializer.cs
@@ -0,0 +1,108 @@
+using Newtonsoft.Json;
+
+// ErrorResponse Class:
+class ErrorResponse
+{
+    public int Status { get; set; }
+    public string Message { get; set; }
+}
+
+// LoginResponse Class:
+class LoginResponse
+{
+    public int Status { get; set; }
+}
+
+// SignupResponse Class:
+class SignupResponse
+{
+    public int Status { get; set; }
+}
+
+// LogoutResponse Class:
+class LogoutResponse
+{
+    public int Status { get; set; }
+}
+
+// GetRoomsResponse Class:
+class GetRoomsResponse
+{
+    public int Status { get; set; }
+    public string Rooms { get; set; }
+}
+
+// GetPlayersInRoomResponse Class:
+class GetPlayersInRoomResponse
+{
+    public int Status { get; set; }
+    public string PlayersInRoom { get; set; }
+}
+
+// GetHighScoreResponse Class:
+class GetHighScoreResponse
+{
+    public int Status { get; set; }
+    public string HighScores { get; set; }
+}
+
+// GetPersonalStatsResponse Class:
+class GetPersonalStatsResponse
+{
+    public int Status { get; set; }
+    public string Statistics { get; set; }
+}
+
+// JoinRoomResponse Class:
+class JoinRoomResponse
+{
+    public int Status { get; set; }
+}
+
+// CreateRoomResponse Class:
+class CreateRoomResponse
+{
+    public int Status { get; set; }
+}
+
+// SearchEloRoomResponse Class:
+class SearchEloRoomResponse
+{
+    public int Status { get; set; }
+    public int RoomID { get; set; }
+}
+
+// GetRoomStateResponse Class:
+class GetRoomStateResponse
+{
+    public int Status { get; set; }
+    public bool IsActive { get; set; }
+    public string Players { get; set; }
+    public string CurrentMove { get; set; }
+    public string GameMode { get; set; }
+}
+
+// LeaveRoomResponse Class:
+class LeaveRoomResponse
+{
+    public int Status { get; set; }
+}
+
+// SubmitMoveResponse Class:
+class SubmitMoveResponse
+{
+    public int Status { get; set; }
+}
+
+class Deserializer
+{
+    // Constants:
+    public const int ERROR_RESPONSE = 2;
+
+    // Methods:
+    public static T DeserializeResponse<T>(string buffer)
+    {
+        // Building the response:
+        return JsonConvert.DeserializeObject<T>(buffer.Substring(1));
+    }
+}
---
+++ Frontend/Assets/Scripts/General/Serializer.cs
@@ -0,0 +1,85 @@
+using Newtonsoft.Json;
+
+// LoginRequest Class:
+class LoginRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
+
+// SignupRequest Class:
+class SignupRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+}
+
+// LogoutRequest Class:
+class LogoutRequest { }
+
+// GetRoomsRequest Class:
+class GetRoomsRequest { }
+
+// GetPlayersInRoomRequest Class:
+class GetPlayersInRoomRequest
+{
+    public int RoomID { get; set; }
+}
+
+// GetHighScoreRequest Class:
+class GetHighScoreRequest { }
+
+// GetPersonalStatsRequest Class:
+class GetPersonalStatsRequest { }
+
+// JoinRoomRequest Class:
+class JoinRoomRequest
+{
+    public int RoomID { get; set; }
+}
+
+// CreateRoomRequest Class:
+class CreateRoomRequest
+{
+    public string GameMode { get; set; }
+}
+
+// GetRoomStateRequest Class:
+class SearchEloRoomRequest { }
+
+// GetRoomStateRequest Class:
+class GetRoomStateRequest { }
+
+// LeaveRoomRequest Class:
+class LeaveRoomRequest { }
+
+// SubmitMoveRequest Class:
+class SubmitMoveRequest { }
+
+static class Serializer
+{
+    // Constants:
+    public const int LOGIN_REQUEST = 101;
+    public const int SIGNUP_REQUEST = 102;
+    public const int LOGOUT_REQUEST = 103;
+    public const int GET_ROOMS_REQUEST = 104;
+    public const int GET_PLAYERS_IN_ROOM_REQUEST = 105;
+    public const int GET_HIGH_SCORE_REQUEST = 106;
+    public const int GET_PERSONAL_STATS_REQUEST = 107;
+    public const int JOIN_ROOM_REQUEST = 108;
+    public const int CREATE_ROOM_REQUEST = 109;
+    public const int SEARCH_ELO_ROOM_REQUEST = 110;
+    public const int GET_ROOM_STATE_REQUEST = 111;
+    public const int LEAVE_ROOM_REQUEST = 112;
+    public const int SUBMIT_MOVE_REQUEST = 113;
+
+
+    // Methods:
+
+    public static string SerializeRequest<T>(T request, int requestCode)
+    {
+        // Returning the buffer:
+        return (char)requestCode + JsonConvert.SerializeObject(request);
+    }
+}
